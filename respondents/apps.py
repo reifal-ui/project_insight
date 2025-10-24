2c671e0d2cbfd8010cdd317af0eccabf5750b233
Rei <mac@MacBook-Pro.local>
2025-10-24 09:20:01 +0700

    FE DAN BE

--- respondents/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class RespondentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'respondents'
-    verbose_name = 'Respondent Management'
--- surveys/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Survey, SurveyAnalytics
-
-@receiver(post_save, sender=Survey)
-def create_survey_analytics(sender, instance, created, **kwargs):
-    """Auto create analytics when survey is created"""
-    if created:
-        SurveyAnalytics.objects.get_or_create(survey=instance)
